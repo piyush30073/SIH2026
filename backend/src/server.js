1f7e5cb80bb0d03e634e6aa1c49032aeee326f58
piyush30073 <piyush30073@gmail.com>
2026-08-15 10:49:25 +0530

    connected database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 import "dotenv/config";
 import app from "./app.js";
+import connectDB from "./config/database.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`SIH2026 backend running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`SIH2026 backend running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
